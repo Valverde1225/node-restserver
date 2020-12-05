0f233c124a77d9f0fb91046b27541b26aed48c6d
Ivan <ivanvalverdedroguett@gmail.com>
2020-12-04 23:14:03 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -17,6 +17,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/CafeDB';
 } else {
-    urlDB = 'mongodb+srv://ivanadm:vd1Vm14MQ0Nm5h1X@cluster0.g9svp.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
