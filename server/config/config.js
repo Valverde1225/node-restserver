7f8bb73ac52886154ae5d080e054ef6b680c0105
Ivan <ivanvalverdedroguett@gmail.com>
2020-12-04 22:26:06 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,21 @@
 // Puerto
 // ===============
 process.env.PORT = process.env.PORT || 3000;
+
+
+// ===============
+// Entorno
+// ===============
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+// ===============
+// Base de datos
+// ===============
+let urlDB;
+
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/CafeDB';
+} else {
+    urlDB = 'mongodb+srv://ivanadm:vd1Vm14MQ0Nm5h1X@cluster0.g9svp.mongodb.net/cafe?retryWrites=true&w=majority';
+}
+process.env.URLDB = urlDB;
